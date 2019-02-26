d6ec8e3521f32e5100a83c67025090da373d1603
srinivassrini <srinivassena8@gmail.com>
2019-02-26 12:36:06 +0530

    update

---
+++ fac1.py
@@ -0,0 +1,6 @@
+def add(t1,t2):
+    den=t1[1]*t2[1]
+    num=t1[0]*t2[1]+t1[1]*t2[0]
+    return num,den
+ 
+
---
+++ tst4.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b:
+    print("a is greater")
+elif b>c:
+    print('b is greater')
+else:
+    print('c is greater')
